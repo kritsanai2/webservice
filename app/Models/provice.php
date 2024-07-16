ba6cc4ad832e6a9df550be2d341f03b3c15dc9eb
june <gfc20140@gmail.com>
2024-07-16 13:56:15 +0700

    k11

---
+++ app/Models/provice.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class provice extends Model
+{
+    use HasFactory;
+}
